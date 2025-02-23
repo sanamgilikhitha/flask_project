3f8f4234796d9a0d8cd848cd03d15ab6418f39f7
slikhitha <likhitha0472@gmail.com>
2025-02-23 13:51:10 +0530

    string change

--- debugging.py
+++ debugging.py
@@ -6,7 +6,7 @@ def  hello_world():
     import pdb; pdb.set_trace() #execute line by line in vscode
     i = 3 
     i = i + 2 
-    return 'Hello World' + str(i) + "times"
+    return "how are you" + str(i) + "times" 
 
 if __name__ == '__main__':
     app.debug = True #it shows what is error 
