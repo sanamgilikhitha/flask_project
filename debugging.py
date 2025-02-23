6cb13f6335621a22d733331c02ff5b6c3a8160bf
slikhitha <likhitha0472@gmail.com>
2025-02-23 14:04:33 +0530

    changed

--- debugging.py
+++ debugging.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def  hello_world():
     import pdb; pdb.set_trace() #execute line by line in vscode
-    i = 3 
+    i = 5
     return "give me " + str(i) + "times" 
 
 if __name__ == '__main__':
